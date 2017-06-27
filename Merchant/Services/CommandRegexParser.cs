97d38efecc14ac6cf73bf205a22116afcb947f40
Marcos <marcostamashiro@hotmail.com>
2017-06-27 13:44:38 -0300

    New parser using regex

---
+++ Merchant/Services/CommandRegexParser.cs
@@ -0,0 +1,55 @@
+﻿using Merchant.Interfaces;
+using Merchant.Models;
+using System;
+using System.Text.RegularExpressions;
+
+namespace Merchant.Services
+{
+    /// <summary>
+    /// Parse the text commands using regex
+    /// </summary>
+    public class CommandRegexParser : ICommandParser
+    {
+        private const string _manyPattern = @"^ *how +many +(?<Target>\w+) +is +(?<AlienValue>.*\b) +(?<Commodity>\w+) *\? *$";
+        private const string _mapPattern = @"^ *(?<From>\w+) +is +(?<To>\w+) *$";
+        private const string _muchPattern = @"^ *how +much +is +(?<AlienValue>.*\b) *\? *$";
+        private const string _ratePattern = @"^ *(?<AlienValue>.*) +(?<Commodity>\w+) +is +(?<TargetValue>\d+) +(?<Target>\w+) *$";
+        private readonly Regex _manyExpression = new Regex(_manyPattern, RegexOptions.CultureInvariant & RegexOptions.IgnoreCase);
+        private readonly Regex _mapExpression = new Regex(_mapPattern, RegexOptions.CultureInvariant & RegexOptions.IgnoreCase);
+        private readonly Regex _muchExpression = new Regex(_muchPattern, RegexOptions.CultureInvariant & RegexOptions.IgnoreCase);
+        private readonly Regex _rateExpression = new Regex(_ratePattern, RegexOptions.CultureInvariant & RegexOptions.IgnoreCase);
+
+        /// <summary>
+        /// Parse the text and returns a command
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public Command Parse(string text)
+        {
+            if (_mapExpression.IsMatch(text))
+            {
+                var result = _mapExpression.Matches(text);
+                return new MapCommand(result[0].Groups["From"].Value, result[0].Groups["To"].Value);
+            }
+            else if (_manyExpression.IsMatch(text))
+            {
+                var result = _manyExpression.Matches(text);
+                if (!"Credits".Equals(result[0].Groups["Target"].Value, StringComparison.OrdinalIgnoreCase)) return new UnknownCommand(text);
+                return new ManyQueryCommand(result[0].Groups["Commodity"].Value, result[0].Groups["AlienValue"].Value);
+            }
+            else if (_muchExpression.IsMatch(text))
+            {
+                var result = _muchExpression.Matches(text);
+                return new MuchQueryCommand(result[0].Groups["AlienValue"].Value);
+            }
+            else if (_rateExpression.IsMatch(text))
+            {
+                var result = _rateExpression.Matches(text);
+                if (!"Credits".Equals(result[0].Groups["Target"].Value, StringComparison.OrdinalIgnoreCase)) return new UnknownCommand(text);
+                var targetValue = int.Parse(result[0].Groups["TargetValue"].Value);
+                return new RateCommand(result[0].Groups["Commodity"].Value, result[0].Groups["AlienValue"].Value, targetValue);
+            }
+            return new UnknownCommand(text);
+        }
+    }
+}
---
+++ UnitTests/CommandRegexParserTests.cs
@@ -0,0 +1,86 @@
+﻿using Merchant.Services;
+using Merchant.Models;
+using System;
+using Xunit;
+
+namespace UnitTests
+{
+    public class CommandRegexParserTests
+    {
+        private const string Category = "CommandRegexParser";
+
+        private readonly CommandRegexParser _target = new CommandRegexParser();
+
+        [Fact]
+        [Trait("Category", Category)]
+        public void TestManyQueryCommand()
+        {
+            // Arrange
+            string input = "how many Credits is glob prok Iron ?";
+            // Act
+            var output = _target.Parse(input);
+            // Assert
+            Assert.IsType<ManyQueryCommand>(output);
+            var final = output as ManyQueryCommand;
+            Assert.Equal<string>("glob prok", final.AlienValue);
+            Assert.Equal<string>("Iron", final.Commodity);
+        }
+
+        [Fact]
+        [Trait("Category", Category)]
+        public void TestMapCommand()
+        {
+            // Arrange
+            string input = "glob is I";
+            // Act
+            var output = _target.Parse(input);
+            // Assert
+            Assert.IsType<MapCommand>(output);
+            var final = output as MapCommand;
+            Assert.Equal<string>("glob", final.From);
+            Assert.Equal<string>("I", final.To);
+        }
+
+        [Fact]
+        [Trait("Category", Category)]
+        public void TestMuchQueryCommand()
+        {
+            // Arrange
+            string input = "how much is pish tegj glob glob ?";
+            // Act
+            var output = _target.Parse(input);
+            // Assert
+            Assert.IsType<MuchQueryCommand>(output);
+            var final = output as MuchQueryCommand;
+            Assert.Equal<string>("pish tegj glob glob", final.AlienValue);
+        }
+
+        [Fact]
+        [Trait("Category", Category)]
+        public void TestRateCommand()
+        {
+            // Arrange
+            string input = "glob glob Silver is 34 Credits";
+            // Act
+            var output = _target.Parse(input);
+            // Assert
+            Assert.IsType<RateCommand>(output);
+            var final = output as RateCommand;
+            Assert.Equal<string>("glob glob", final.Amount);
+            Assert.Equal<String>("Silver", final.Commodity);
+            Assert.Equal<int>(34, final.CreditValue);
+        }
+
+        [Fact]
+        [Trait("Category", Category)]
+        public void TestUnknownCommand()
+        {
+            // Arrange
+            string input = "how much wood could a woodchuck chuck if a woodchuck could chuck wood ?";
+            // Act
+            var output = _target.Parse(input);
+            // Assert
+            Assert.IsType<UnknownCommand>(output);
+        }
+    }
+}
