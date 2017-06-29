eba62f4d049a33a8a52faf4e8486b6d868b0c1df
Marcos <marcostamashiro@hotmail.com>
2017-06-29 16:49:48 -0300

    NotEmpty extension more generic

---
+++ Merchant/Extensions/ICollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+
+namespace Merchant.Extensions
+{
+    /// <summary>
+    /// Extension methods for collection
+    /// </summary>
+    public static class ICollectionExtensions
+    {
+        /// <summary>
+        /// Check if it is empty
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static bool NotEmpty(this ICollection value)
+        {
+            return value.Count > 0;
+        }
+    }
+}
